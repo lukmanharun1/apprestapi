17d277152afeb441d1604f48364cdcc635d051e2
fajriputra <fajriwn27@gmail.com>
2021-08-02 15:26:27 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -10,9 +10,21 @@ exports.index = function (req, res) {
 exports.tampilsemuadatamahasiswa = function (req, res) {
     connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
         if (error) {
-            connection.log(error);
+            cconsole.log(error);
         } else {
             response.ok(rows, res);
         }
     });
 }
+
+// menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function  (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id], function(error, rows, fields) {
+        if (error) {
+            console.log(error)
+        } else {
+            response.ok(rows, res);
+        }
+    });    
+};
--- routes.js
+++ routes.js
@@ -5,4 +5,8 @@ module.exports = function (app) {
     app.route('/')
         .get(jsonku.index);
     app.route('/tampil').get(jsonku.tampilsemuadatamahasiswa);
+
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid);
 }
+
