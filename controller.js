47b6ac88a8095978815276e929b09edd8409196c
fajriputra <fajriwn27@gmail.com>
2021-08-02 15:14:06 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -4,6 +4,15 @@ const response = require('./res');
 const connection = require('./koneksi');
 
 exports.index = function (req, res) {
-    response.ok('Aplikasi REST API ku berjalan')
+    response.ok('Aplikasi REST API ku berjalan', res)
     
+}
+exports.tampilsemuadatamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
 }
