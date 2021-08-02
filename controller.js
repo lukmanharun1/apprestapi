01c8c3adcfad0fd86dd2854cbf1098db706b0f35
fajriputra <fajriwn27@gmail.com>
2021-08-02 14:28:55 +0700

    membuat file controller

---
+++ controller.js
@@ -0,0 +1,9 @@
+'use-strict';
+
+const response = require('./res');
+const connection = require('./koneksi');
+
+exports.index = function (req, res) {
+    response.ok('Aplikasi REST API ku berjalan')
+    
+}
