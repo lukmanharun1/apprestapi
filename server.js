c1720b26c190279dafaaa389cebf6919f4ba0edf
fajriputra <fajriwn27@gmail.com>
2021-08-03 11:14:04 +0700

    registrasi berhasil

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const morgan = require('morgan');
 const app = express();
 
 // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // memanggil routes
 const routes = require('./routes');
 routes(app);
+
+// daftarkan menu routes index
+app.use('/auth', require('./middleware'));
 
 app.listen(3000, () => {
     console.log('Server started on 3000');
